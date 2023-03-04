e2eb4c801784644855ab2385ffd62265a5c5b459
zemscky <aleksei.zemscky@yandex.ru>
2023-03-04 16:01:09 +0300

    Добавил метод setPassword

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -2,7 +2,9 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UserDto;
 
 @CrossOrigin(value = "http://localhost:3000")
@@ -13,5 +15,9 @@ public class UserController {
     @PatchMapping("/{userId}")
     public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         return ResponseEntity.ok(new UserDto());
+    }
+    @PostMapping("/set_password")
+    public ResponseEntity<NewPasswordDto> setPassword(@RequestBody NewPasswordDto newPasswordDto) {
+        return ResponseEntity.ok(newPasswordDto);
     }
 }
