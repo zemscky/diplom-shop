aa9ff72cf47154265e129db85da117bb39e96219
Ivan <ivan90kiseliov@gmail.com>
2023-03-04 19:10:43 +0300

    Созданы классы ads user adscomment

---
+++ src/main/java/ru/skypro/homework/entity/AdsComment.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+//@Table(name = "ads_comment")
+@NoArgsConstructor
+@EqualsAndHashCode
+@AllArgsConstructor
+@Getter
+@Setter
+public class AdsComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+}
