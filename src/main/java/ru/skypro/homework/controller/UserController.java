4e7bb6c6b4e220bd954f72c150a26b31cda5279b
Ivan <ivan90kiseliov@gmail.com>
2023-03-04 19:30:22 +0300

    добавил getUsers и сервис юзера

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -3,21 +3,27 @@ package ru.skypro.homework.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDto;
 import ru.skypro.homework.dto.UserDto;
+import ru.skypro.homework.entity.User;
+import ru.skypro.homework.service.UserService;
+
+import java.util.Collection;
 
 @CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     @PatchMapping("/me")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
         return ResponseEntity.ok(new UserDto());
     }
+
     @PostMapping("/set_password")
     public ResponseEntity<NewPasswordDto> setPassword(@RequestBody NewPasswordDto newPasswordDto) {
         return ResponseEntity.ok(newPasswordDto);
@@ -26,5 +32,13 @@ public class UserController {
     @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<MultipartFile> updateUserImage(@RequestParam MultipartFile image) {
         return ResponseEntity.ok(image);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getUsers() {
+
+        Collection<User> users = userService.getUsers();
+//временно так
+        return ResponseEntity.ok(new UserDto());
     }
 }
---
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -0,0 +1,11 @@
+package ru.skypro.homework.service;
+
+import ru.skypro.homework.entity.User;
+
+import java.util.Collection;
+
+public interface UserService {
+    Collection<User> getUsers();
+
+}
+
