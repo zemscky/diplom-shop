0fffbfe576ff94f86642360518da9fcb103ead58
zemscky <aleksei.zemscky@yandex.ru>
2023-03-04 18:10:26 +0300

    Исправил UserController, добавил методы в AdsController (getAdsComments, addAdsComments).

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.AdsCommentDto;
 import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.dto.ResponseWrapperAdsComment;
 import ru.skypro.homework.service.AdsService;
 
+import java.util.ArrayList;
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
 @RequiredArgsConstructor
@@ -20,5 +23,15 @@ public class AdsController {
     public ResponseEntity<AdsDto> addAds(@PathVariable("id") Long userId,
                                          @RequestBody AdsDto adsDto) {
         return ResponseEntity.ok(new AdsDto());
+    }
+    @GetMapping("/{ad_pk}/comments")
+    public ResponseWrapperAdsComment <AdsCommentDto> getAdsComments(@PathVariable("ad_pk") long adPk) {
+        return ResponseWrapperAdsComment.of(new ArrayList<>());
+    }
+
+    @PostMapping("/{ad_pk}/comments")
+    public ResponseEntity<AdsCommentDto> addAdsComments(@PathVariable("ad_pk") long adPk,
+                                                        @RequestBody AdsCommentDto adsCommentDto) {
+        return ResponseEntity.ok(new AdsCommentDto());
     }
 }
