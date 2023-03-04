6dc67b9ce77442c1f5f6f9cff16a35d26d7e97fc
Ирина <mi.irene97@gmail.com>
2023-03-04 15:59:14 +0300

    Подправила предыдущий коммит

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -2,23 +2,23 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.AdsDto;
 import ru.skypro.homework.service.AdsService;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
-@RestController
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/ads")
 public class AdsController {
 
     private final AdsService adsService;
 
-    @PostMapping("/new-ad")
-    public AdsDto addAds(@RequestBody AdsDto adsDto) {
-        return new AdsDto();
+    @PostMapping("/{id}/new-ad")
+    public ResponseEntity<AdsDto> addAds(@PathVariable("id") Long userId,
+                                         @RequestBody AdsDto adsDto) {
+        return ResponseEntity.ok(new AdsDto());
     }
 }
