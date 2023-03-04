bc3d4d0e7be326a73e0f7274587a1378b1749876
Ирина <mi.irene97@gmail.com>
2023-03-04 17:59:26 +0300

    Добавила методы, работающие с объявлениями

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.AdsDto;
+import ru.skypro.homework.dto.FullAdsDto;
+import ru.skypro.homework.dto.ResponseWrapperAds;
 import ru.skypro.homework.service.AdsService;
 
 @Slf4j
@@ -16,9 +18,36 @@ public class AdsController {
 
     private final AdsService adsService;
 
-    @PostMapping("/{id}/new-ad")
-    public ResponseEntity<AdsDto> addAds(@PathVariable("id") Long userId,
+    @GetMapping
+    public ResponseWrapperAds getAllAds() {
+        return new ResponseWrapperAds();
+    }
+
+    @GetMapping("/me{userId}")
+    public ResponseWrapperAds getAdsMe(@PathVariable("userId") Long userId) {
+        return new ResponseWrapperAds();
+    }
+
+    @GetMapping("/{adId}") // Разобраться: айди юзера или рекламного объявления. Я склоняюсь к тому,
+    // что, судя по названию и возвращаемому значению в файле openapi, это ПОЛНОЕ объявление.
+    // То есть выбираем из списка и открываем.
+    public ResponseEntity<FullAdsDto> getFullAd(@PathVariable("adId") Long adId) {
+        return ResponseEntity.ok(new FullAdsDto());
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<AdsDto> addAds(@PathVariable("userId") Long userId,
                                          @RequestBody AdsDto adsDto) {
         return ResponseEntity.ok(new AdsDto());
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<AdsDto> updateAds(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok(new AdsDto());
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> removeAds(@PathVariable("userId") Long userId) {
+        return ResponseEntity.ok("Объявление успешно удалено");
     }
 }
