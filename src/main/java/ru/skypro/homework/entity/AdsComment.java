756d7ad906b3fbbb973a91f04279387d6bb1f964
AnastasiaSergeeva05 <112942778+AnastasiaSergeeva05@users.noreply.github.com>
2023-03-06 14:47:51 +0300

    добавлены поля для AdsComment

--- src/main/java/ru/skypro/homework/entity/AdsComment.java
+++ src/main/java/ru/skypro/homework/entity/AdsComment.java
@@ -1,5 +1,6 @@
 package ru.skypro.homework.entity;
 
+import com.sun.istack.NotNull;
 import lombok.*;
 
 import javax.persistence.*;
@@ -17,5 +18,20 @@ public class AdsComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "id_ads")
+    private Ads ads;
+
+    @Column(name = "comment_time")
+    private LocalDateTime createdAt;
+
+    @Column(name = "comment_text")
+    @NotNull
+    private String text;
 
 }
