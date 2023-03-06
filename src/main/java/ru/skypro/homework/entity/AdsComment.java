31fd57eaf4c8c62ca5ec1dc240b17e9702d5e2e9
letuu <albertvt@ya.ru>
2023-03-06 23:55:26 +0300

    Entity заполн, связи, доб Image; Доб аннотац в AdsCommentDto,AdsDto,UserDto; Доб Regexp

--- src/main/java/ru/skypro/homework/entity/AdsComment.java
+++ src/main/java/ru/skypro/homework/entity/AdsComment.java
@@ -1,12 +1,13 @@
 package ru.skypro.homework.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-//@Table(name = "ads_comment")
+//@Table(name = "ads_comments")
 @NoArgsConstructor
 @EqualsAndHashCode
 @AllArgsConstructor
@@ -17,5 +18,19 @@ public class AdsComment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    private LocalDateTime createdAt;
+
+    private String text;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "pk_ads")
+    private Ads ad;
 
 }
--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -1,11 +1,14 @@
 package ru.skypro.homework.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
 import ru.skypro.homework.dto.Role;
 
 import javax.persistence.*;
+import java.util.List;
 
-//@Entity
+@Entity
 //@Table(name = "users")
 @NoArgsConstructor
 @EqualsAndHashCode
@@ -28,7 +31,17 @@ public class User {
 
     private String phone;
 
+    private String image;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
+    @JsonManagedReference //не уверен, возможно @JsonBackReference
+    private List<Ads> ads;
+
+    @OneToMany(mappedBy = "author")
+    @JsonBackReference
+    List<AdsComment> adsComment;
 
 }
