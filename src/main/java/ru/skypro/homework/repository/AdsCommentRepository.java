98ceaf48b70b6cc25a61a6dc8c7e73473761feab
AnastasiaSergeeva05 <112942778+AnastasiaSergeeva05@users.noreply.github.com>
2023-03-06 14:45:23 +0300

    create  interface AdsCommentRepository

---
+++ src/main/java/ru/skypro/homework/repository/AdsCommentRepository.java
@@ -0,0 +1,14 @@
+package ru.skypro.homework.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.skypro.homework.entity.AdsComment;
+
+import java.util.Collection;
+import java.util.Optional;
+
+
+public interface AdsCommentRepository extends JpaRepository<AdsComment, Long> {
+
+    Collection<AdsComment> findAllByAdsId(long adsId);
+
+}
