b9f5f1b71f50c2590795adbe16d337b1e0fc5c89
nikitamavrodiy <mna1998@mail.ru>
2023-03-08 11:53:10 +0300

    добавил UserServiceImpl

---
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.service.impl;
+
+import ru.skypro.homework.entity.User;
+import ru.skypro.homework.service.UserService;
+
+import java.util.Collection;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public Collection<User> getUsers() {
+        return null;
+    }
+}
