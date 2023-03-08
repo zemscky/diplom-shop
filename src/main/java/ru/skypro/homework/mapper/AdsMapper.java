327e5e12a2a20b80f31e7e0252a712b370870d0b
nikitamavrodiy <mna1998@mail.ru>
2023-03-08 11:57:54 +0300

    убрал лишние методы и неиспользуемые импорты

--- src/main/java/ru/skypro/homework/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsMapper.java
@@ -6,16 +6,9 @@ import ru.skypro.homework.dto.AdsDto;
 import ru.skypro.homework.dto.CreateAdsDto;
 import ru.skypro.homework.dto.FullAdsDto;
 import ru.skypro.homework.entity.Ads;
-import ru.skypro.homework.entity.Image;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface AdsMapper extends WebMapper<AdsDto, Ads> {
-
     @Mapping(target = "id", source = "pk")
     @Mapping(target = "author.id", source = "author")
     @Mapping(target = "images", source = "imagesDto")
@@ -40,8 +33,4 @@ public interface AdsMapper extends WebMapper<AdsDto, Ads> {
     @Mapping(target = "imagesDto", source = "images")
     @Mapping(target = "pk", source = "id")
     FullAdsDto toFullAdsDto(Ads entity);
-
-    Collection<Ads> toEntityList(Collection<AdsDto> adsDtoList); //это обязательно?
-
-    Collection<AdsDto> toDtoList(Collection<Ads> adsList);//это обязательно?
 }
