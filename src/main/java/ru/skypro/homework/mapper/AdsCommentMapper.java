54f068e175c678237b035861b3d6e4a75406c84b
nikitamavrodiy <mna1998@mail.ru>
2023-03-10 16:19:28 +0300

    фикс маппера AdsCommentMapper (поле createdAt - Instant)

--- src/main/java/ru/skypro/homework/mapper/AdsCommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdsCommentMapper.java
@@ -8,13 +8,12 @@ import ru.skypro.homework.entity.AdsComment;
 @Mapper
 public interface AdsCommentMapper extends WebMapper<AdsCommentDto, AdsComment>{
     @Mapping(target = "author", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "id", source = "pk")
     @Mapping(target = "ad", ignore = true)
     AdsComment toEntity(AdsCommentDto dto);
 
     @Mapping(target = "author", source = "author.id")
     @Mapping(source = "id", target = "pk")
-    @Mapping(target = "createdAt", source = "entity.createdAt", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping(target = "createdAt", source = "entity.createdAt")
     AdsCommentDto toDto(AdsComment entity);
 }
