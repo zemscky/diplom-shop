c5f9a302505529418b5cc695aada31c046129fab
nikitamavrodiy <mna1998@mail.ru>
2023-03-12 21:48:31 +0300

    Реализованы getComments и updateComments,

--- src/main/java/ru/skypro/homework/service/AdsService.java
+++ src/main/java/ru/skypro/homework/service/AdsService.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import ru.skypro.homework.dto.*;
+import ru.skypro.homework.entity.AdsComment;
 
 public interface AdsService {
 
@@ -23,21 +24,36 @@ public interface AdsService {
      * Finds an Ads instance in the repository by its id and converts it into
      * a FullAdsDto instance. The purpose is to provide the user with the
      * full information on the chosen advertisement.
-     * @param adId
+     * @param adId Ad ID
      * @return ResponseEntity<FullAdsDto>
      */
     ResponseEntity<FullAdsDto> getFullAd(Long adId);
     ResponseEntity<AdsDto> addAds(Long userId, AdsDto adsDto);
-    ResponseEntity<AdsCommentDto> getComments(int adPk, int id);
+    /**
+     * Getting a comment by ID
+     *
+     * @param id    Comment ID
+     * @param adPk  Ad ID
+     * @return      Found comment
+     */
+    AdsComment getAdsComment(long adPk, long id);
 
     /**
      * Deletes a comment
-     * @param adPk Ads id
-     * @param id AdsComment id
-     * @return ResponseEntity<HttpStatus>
+     * @param   adPk    Ads id
+     * @param   id      AdsComment id
+     * @return  ResponseEntity<HttpStatus>
      */
     ResponseEntity<HttpStatus> deleteComments(long adPk, long id);
-    ResponseEntity<AdsCommentDto> updateComments(int adPk, int id, AdsCommentDto adsCommentDto);
+    /**
+     * Changing the comment by ID
+     *
+     * @param id            Comment id
+     * @param adPk          Ad id
+     * @param adsComment    Modified comment
+     * @return Modified comment
+     */
+    AdsComment updateComments(int adPk, int id, AdsComment adsComment);
     ResponseEntity<AdsDto> updateAds(Long userId);
     ResponseEntity<String> removeAds(Long userId);
     ResponseWrapper<AdsCommentDto> getAdsComments(long adPk);
