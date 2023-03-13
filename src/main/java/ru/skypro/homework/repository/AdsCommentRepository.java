0469476d187e505d45cef226068c8137e63d59a5
nikitamavrodiy <mna1998@mail.ru>
2023-03-13 22:12:41 +0300

    добавил метод deleteAdsCommentsByAdsId в репозиторий, аннотации @Modifying @Query

--- src/main/java/ru/skypro/homework/repository/AdsCommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/AdsCommentRepository.java
@@ -2,6 +2,8 @@ package ru.skypro.homework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.AdsComment;
 
@@ -15,4 +17,8 @@ public interface AdsCommentRepository extends JpaRepository<AdsComment, Long> {
     Optional<AdsComment> findByIdAndAdId(long id, long adsId);
 
     Collection<AdsComment> findAllByAdId(long adId);
+
+    @Modifying
+    @Query("delete FROM AdsComment a where a.ad.id =: id")
+    void deleteAdsCommentsByAdsId(long id);
 }
