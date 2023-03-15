e7c501f05b15deb473e0f231ba62bdea1c01440d
nikitamavrodiy <mna1998@mail.ru>
2023-03-15 17:02:50 +0300

    passwordEncoder, userDetailsService, newPassword  в UserServiceImpl

--- src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -1,25 +1,33 @@
 package ru.skypro.homework.service.impl;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import ru.skypro.homework.dto.UserDto;
 import ru.skypro.homework.entity.User;
-import ru.skypro.homework.mapper.UserMapper;
 import ru.skypro.homework.repository.UserRepository;
+import ru.skypro.homework.security.UserDetailsServiceImpl;
 import ru.skypro.homework.service.UserService;
 
 import java.util.Collection;
 
+import static ru.skypro.homework.security.SecurityUtils.getUserDetailsFromContext;
+
 
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
 
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+    private final UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.userMapper = userMapper;
     }
 
     @Override
@@ -46,4 +54,18 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.save(user);
     }
+
+    @Override
+    public void newPassword(String newPassword, String currentPassword) {
+
+        UserDetails userDetails = getUserDetailsFromContext();
+
+        if (!passwordEncoder.matches(currentPassword, userDetails.getPassword())) {
+            throw new BadCredentialsException("Неверно указан текущий пароль!");
+        }
+
+        userDetailsService.updatePassword(userDetails, passwordEncoder.encode(newPassword));
+    }
+
+
 }
