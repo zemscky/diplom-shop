dc83cb5aaa6c0c0ffc7cfc625e1d439b1a54fe83
nikitamavrodiy <mna1998@mail.ru>
2023-03-15 16:27:51 +0300

    комментарии в интерфейсе

--- src/main/java/ru/skypro/homework/service/AuthService.java
+++ src/main/java/ru/skypro/homework/service/AuthService.java
@@ -3,7 +3,19 @@ package ru.skypro.homework.service;
 import ru.skypro.homework.dto.RegisterReqDto;
 import ru.skypro.homework.dto.Role;
 
+/**
+ * Сервис для регистрации пользователя и входа
+ */
 public interface AuthService {
+
+    /**
+     * @param userName Логин (email)
+     * @param password Пароль
+     */
     boolean login(String userName, String password);
+
+    /**
+     * @param role Роль пользователя
+     */
     boolean register(RegisterReqDto registerReqDto, Role role);
 }
