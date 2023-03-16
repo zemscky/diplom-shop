8e985a81f8ba287dcc7ae8b67eb66fc0c568d087
nikitamavrodiy <mna1998@mail.ru>
2023-03-16 16:47:15 +0300

    adsDto поменял местами поля, jsonIgnore аннотацию на поле description

--- src/main/java/ru/skypro/homework/dto/AdsDto.java
+++ src/main/java/ru/skypro/homework/dto/AdsDto.java
@@ -1,5 +1,6 @@
 package ru.skypro.homework.dto ;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
@@ -7,18 +8,19 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class AdsDto {
 
+    private int author;
+
+    private byte[] image;
+
     private int pk;
+
+    private int price;
 
     @NotBlank
     private String title;
 
     @NotBlank
+    @JsonIgnore
     private String description;
-
-    private byte[] image;
-
-    private int price;
-
-    private int author;
 
 }
