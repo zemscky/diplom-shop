6f9fb110722f13c205c6c64307580412a65c45dd
nikitamavrodiy <mna1998@mail.ru>
2023-03-22 12:52:29 +0300

    AdsControllerTest.java

--- src/test/java/ru/skypro/homework/controller/AdsControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/AdsControllerTest.java
@@ -2,18 +2,12 @@ package ru.skypro.homework.controller;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import ru.skypro.homework.repository.AdsRepository;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
