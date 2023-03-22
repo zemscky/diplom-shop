abcb5a9a3c85a8f033613ea868247a1010826597
Ирина <mi.irene97@gmail.com>
2023-03-22 20:07:33 +0300

    тест на getAdsMe

--- src/test/java/ru/skypro/homework/controller/AdsControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/AdsControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockPart;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -85,6 +86,7 @@ public class AdsControllerTest {
     }
 
     @Test
+    @WithMockUser
     void getAdsMe() throws Exception {
         mockMvc.perform(get("/ads/me"))
                 .andDo(print())
